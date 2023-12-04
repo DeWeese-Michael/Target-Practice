9aebdd1632e11e1228f5344549f6ad2790c66949
Larry Y <larryyu0116@gmail.com>
2023-12-03 20:01:17 -0600

    add a target square

--- Target_Practice/Target_Practice/ViewController.swift
+++ Target_Practice/Target_Practice/ViewController.swift
@@ -10,38 +10,50 @@ import SceneKit
 import ARKit
 import Vision
 
+import FocusNode
+import SmartHitTest
+
+extension ARSCNView: ARSmartHitTest {}
+
+extension ViewController {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        self.focusNode.updateFocusNode()
+    }
+}
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
-    
+    let focusNode = FocusSquare()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+
+        sceneView.frame = self.view.bounds
+        self.view.addSubview(sceneView)
+        self.sceneView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
         sceneView.delegate = self
-        
         sceneView.showsStatistics = true
-        
-        
+
+        self.focusNode.viewDelegate = sceneView
+        sceneView.scene.rootNode.addChildNode(self.focusNode)
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
+
         let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
+        configuration.planeDetection = [.horizontal, .vertical]
+
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
+
         sceneView.session.pause()
     }
-
-
 }
 
